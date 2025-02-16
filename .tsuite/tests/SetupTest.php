b70f3e81a43e28c547a207826f8fc66c9a23d36f
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-16 18:27:16 -0500

    mor efake tests

--- .tsuite/tests/SetupTest.php
+++ .tsuite/tests/SetupTest.php
@@ -10,4 +10,14 @@
         assertEquals(1, 2, '1 should be equal to 2');
     }
 
+    function test_setup_page_3() {
+        echo 'SetupTest::test_setup_page called\n';
+        assertEquals(2, 2, '2 should be equal to 2');
+    }
+
+    function test_setup_page_4() {
+        echo 'SetupTest::test_setup_page called\n';
+        assertEquals(5, 5, '5 should be equal to 5');
+    }
+
 ?>
