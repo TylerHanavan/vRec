9716ff9dd9738e123b902bf08656f6e3ea9f7967
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-16 18:29:42 -0500

    add more fake tests

--- .tsuite/tests/SetupTest.php
+++ .tsuite/tests/SetupTest.php
@@ -1,23 +1,20 @@
 <?php
 
     function test_setup_page() {
-        echo 'SetupTest::test_setup_page called\n';
         assertEquals(1, 1, '1 should be equal to 1');
     }
 
     function test_setup_page_2() {
-        echo 'SetupTest::test_setup_page called\n';
         assertEquals(1, 2, '1 should be equal to 2');
     }
 
     function test_setup_page_3() {
-        echo 'SetupTest::test_setup_page called\n';
         assertEquals(2, 2, '2 should be equal to 2');
     }
 
     function test_setup_page_4() {
-        echo 'SetupTest::test_setup_page called\n';
         assertEquals(5, 5, '5 should be equal to 5');
+        assertTrue(true, 'true should be true');
     }
 
 ?>
