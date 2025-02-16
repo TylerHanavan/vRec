254988d362896a4f13264d938bc3e8a5dd136d01
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-16 18:25:44 -0500

    put in failing test cased

--- .tsuite/tests/SetupTest.php
+++ .tsuite/tests/SetupTest.php
@@ -5,4 +5,9 @@
         assertEquals(1, 1, '1 should be equal to 1');
     }
 
+    function test_setup_page_2() {
+        echo 'SetupTest::test_setup_page called\n';
+        assertEquals(1, 2, '1 should be equal to 2');
+    }
+
 ?>
