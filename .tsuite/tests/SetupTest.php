c82b0452718ed45d240fdc667f6e34b3109471c3
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-16 18:23:33 -0500

    testing an assertEquals

--- .tsuite/tests/SetupTest.php
+++ .tsuite/tests/SetupTest.php
@@ -1,7 +1,8 @@
 <?php
 
     function test_setup_page() {
-        echo 'SetupTest::test_setup_page called';
+        echo 'SetupTest::test_setup_page called\n';
+        assertEquals(1, 1, '1 should be equal to 1');
     }
 
 ?>
