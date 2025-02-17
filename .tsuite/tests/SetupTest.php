87eb14b9fdfa14545fc7a413ebe2e5fa33823e9b
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-16 20:31:26 -0500

    extra test

--- .tsuite/tests/SetupTest.php
+++ .tsuite/tests/SetupTest.php
@@ -6,6 +6,7 @@
 
     function test_setup_page_2() {
         assertEquals(1, 1, '1 should be equal to 1');
+        assertEquals(2, 2, '2 should be equal to 2');
     }
 
     function test_setup_page_3() {
