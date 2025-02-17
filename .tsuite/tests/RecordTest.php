17a1934ed98bbe4a20d9f6fd75eb033b957f58cd
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-16 21:24:30 -0500

    add another test file

---
+++ .tsuite/tests/RecordTest.php
@@ -0,0 +1,28 @@
+<?php
+
+    function test_account_1() {
+        assertEquals(1, 1, '1 should be equal to 1');
+    }
+
+    function test_account_2() {
+        assertEquals(1, 1, '1 should be equal to 1');
+        assertEquals(2, 2, '2 should be equal to 2');
+    }
+
+    function test_account_3() {
+        assertEquals(2, 2, '2 should be equal to 2');
+    }
+
+    function test_account_4() {
+        assertEquals(5, 5, '5 should be equal to 5');
+        assertTrue(true, 'true should be true');
+        assertFalse(false, 'true should be true');
+    }
+
+    function test_account_5() {
+        assertEquals(5, 5, '5 should be equal to 5');
+        assertTrue(true, 'true should be true');
+        assertFalse(false, 'true should be true');
+    }
+
+?>
