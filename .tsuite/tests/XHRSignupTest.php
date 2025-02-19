05456ee17c3b71b1a9a16190356099b247fd5d6a
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-18 20:13:19 -0500

    additional signup xhr tests

--- .tsuite/tests/XHRSignupTest.php
+++ .tsuite/tests/XHRSignupTest.php
@@ -1,12 +1,21 @@
 <?php
 
-    function test_first_setup($properties) {
+    function test_xhr_signup_1($properties) {
         assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
 
         $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array(), true);
 
         assertEquals(400, $response['http_code'], 'http code mismatch');
         assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Missing required fields"}', $response['response'], 'response mismatch');
+    }
+
+    function test_xhr_signup_2($properties) {
+        assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
+
+        $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array('username' => 'ke', 'email' => 'example@example.com', 'password' => 'twelve'), true);
+
+        assertEquals(400, $response['http_code'], 'http code mismatch');
+        assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Username must be between 3 and 50 characters"}', $response['response'], 'response mismatch');
     }
 
     function test_setup_page() {
