f8b9d7c9619008a4abf3110c4706f80599104322
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-18 20:13:58 -0500

    remove fake tests

--- .tsuite/tests/XHRSignupTest.php
+++ .tsuite/tests/XHRSignupTest.php
@@ -18,29 +18,4 @@
         assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Username must be between 3 and 50 characters"}', $response['response'], 'response mismatch');
     }
 
-    function test_setup_page() {
-        assertEquals(1, 1, '1 should be equal to 1');
-    }
-
-    function test_setup_page_2() {
-        assertEquals(1, 1, '1 should be equal to 1');
-        assertEquals(2, 2, '2 should be equal to 2');
-    }
-
-    function test_setup_page_3() {
-        assertEquals(2, 2, '2 should be equal to 2');
-    }
-
-    function test_setup_page_4() {
-        assertEquals(5, 5, '5 should be equal to 5');
-        assertTrue(true, 'true should be true');
-        assertFalse(true, 'true should be true');
-    }
-
-    function test_setup_page_5() {
-        assertEquals(5, 5, '5 should be equal to 5');
-        assertTrue(false, 'true should be true');
-        assertFalse(true, 'true should be true');
-    }
-
 ?>
