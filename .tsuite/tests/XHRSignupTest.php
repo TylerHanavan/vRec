8ae882a7eaccfd10fcf0cd9582fc5678ea74f478
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-19 22:02:26 -0500

    additioinal test for too long username

--- .tsuite/tests/XHRSignupTest.php
+++ .tsuite/tests/XHRSignupTest.php
@@ -18,4 +18,13 @@
         assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Username must be between 3 and 50 characters"}', $response['response'], 'response mismatch');
     }
 
+    function test_xhr_signup_3($properties) {
+        assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
+
+        $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array('username' => 'kekekekekekekekekekekekekekekekekekekekekekekekekekeke', 'email' => 'example@example.com', 'password' => 'twelve'), true);
+
+        assertEquals(400, $response['http_code'], 'http code mismatch');
+        assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Username must be between 3 and 50 characters"}', $response['response'], 'response mismatch');
+    }
+
 ?>
