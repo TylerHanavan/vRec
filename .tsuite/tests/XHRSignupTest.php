b55ef21e9e5573a96130998bfe93cf53caba2b5f
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-19 22:03:29 -0500

    test for invalid email format

--- .tsuite/tests/XHRSignupTest.php
+++ .tsuite/tests/XHRSignupTest.php
@@ -27,4 +27,13 @@
         assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Username must be between 3 and 50 characters"}', $response['response'], 'response mismatch');
     }
 
+    function test_xhr_signup_4($properties) {
+        assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
+
+        $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array('username' => 'kevin', 'email' => 'example@', 'password' => 'twelve'), true);
+
+        assertEquals(400, $response['http_code'], 'http code mismatch');
+        assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Invalid email format"}', $response['response'], 'response mismatch');
+    }
+
 ?>
