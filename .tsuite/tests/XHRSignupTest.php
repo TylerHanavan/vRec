88c325d7cc8062dd8f9aec8b2ad6000879ee3b0b
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-28 07:55:24 -0500

    tidy up table checking code

--- .tsuite/tests/XHRSignupTest.php
+++ .tsuite/tests/XHRSignupTest.php
@@ -6,11 +6,12 @@
         $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array(), true);
 
         assertEquals(200, $response['http_code'], 'http code mismatch');
-        assertStrContains('<p>You are missing the <b>pages</b> table. Generating...</p>', $response['response'], 'response mismatch');
-        assertStrContains('<p>You are missing the <b>accounts</b> table. Generating...</p>', $response['response'], 'response mismatch');
-        assertStrContains('<p>You are missing the <b>sessions</b> table. Generating...</p>', $response['response'], 'response mismatch');
-        assertStrContains('<p>You are missing the <b>record_fields</b> table. Generating...</p>', $response['response'], 'response mismatch');
-        assertStrContains('<p>You are missing the <b>record_definitions</b> table. Generating...</p>', $response['response'], 'response mismatch');
+
+        $tables = array('pages', 'accounts', 'sessions', 'record_fields', 'record_definitions');
+        foreach($tables as $table) {
+            $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array(), true);
+            assertStrContains('<p>You are missing the <b>' . $table . '</b> table. Generating...</p>', $response['response'], 'response mismatch');
+        }
     }
 
     function test_xhr_signup_1($properties) {
