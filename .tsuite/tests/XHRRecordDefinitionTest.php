d36d6e5355d42a0a354b9ea58c3ebd8556bd3af3
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-28 08:24:09 -0500

    switch up debugging

--- .tsuite/tests/XHRRecordDefinitionTest.php
+++ .tsuite/tests/XHRRecordDefinitionTest.php
@@ -25,9 +25,9 @@
         $data['field_name2'] = 't3';
         global $session_token;
         $response = test_curl($properties['endpoint_url'] . '/xhr/new-record-definition', $data, true, $session_token);
-        var_dump($response);
         
         $response_array = json_decode($response['response'], true);
+        var_dump($response_array);
         
         assertTrue(isset($response_array['xhr_table_create_status']), 'xhr_table_create_status not set');
         assertEquals($response_array['xhr_table_create_status'], 'true', 'response status mismatch');
