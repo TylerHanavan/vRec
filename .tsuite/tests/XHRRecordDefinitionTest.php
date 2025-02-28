47798a8a0273654df29637e514693e61ff2a2cd0
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-28 07:43:39 -0500

    try passing cookie to the xhr

--- .tsuite/tests/XHRRecordDefinitionTest.php
+++ .tsuite/tests/XHRRecordDefinitionTest.php
@@ -11,7 +11,23 @@
         $data['field_name2'] = 't3';
         $response = test_curl($properties['endpoint_url'] . '/xhr/new-record-definition', $data, true);
         assertEquals(200, $response['http_code'], 'http code mismatch');
-        var_dump($response);
+        assertStrContains('<!doctype html>', $response['response'], 'response mismatch');
+    }
+
+    function test_xhr_record_definition_2($properties) {
+        $data = array();
+        $data['record_def_name'] = 'test';
+        $data['field_type0'] = 'Text';
+        $data['field_name0'] = 't1';
+        $data['field_type1'] = 'Integer';
+        $data['field_name1'] = 't2';
+        $data['field_type2'] = 'Boolean';
+        $data['field_name2'] = 't3';
+        global $session_token;
+        $response = test_curl($properties['endpoint_url'] . '/xhr/new-record-definition', $data, true, $session_token);
+        var_dump($response['response']);
+        assertEquals(200, $response['http_code'], 'http code mismatch');
+        assertStrContains('<!doctype html>', $response['response'], 'response mismatch');
     }
 
 ?>
