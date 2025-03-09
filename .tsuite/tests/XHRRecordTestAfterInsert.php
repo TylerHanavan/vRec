b31ea0d61a64e40f2bfd23149e635a9fb8d09e31
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-09 11:36:11 -0400

    add id check

--- .tsuite/tests/XHRRecordTestAfterInsert.php
+++ .tsuite/tests/XHRRecordTestAfterInsert.php
@@ -55,7 +55,7 @@
 
         $records = $response_arr['records'];
 
-        $fields = ['t1', 't2', 't3'];
+        $fields = ['id', 't1', 't2', 't3'];
 
         for($x = 0; $x < 3; $x++) {
             assertTrue(isset($records[$x]), "there is no record $x");
@@ -69,19 +69,20 @@
             
         }
 
-        var_dump($records[0]);
-
+        assertEquals(1, $records[0]['id']['value'], 'record 0 id value is wrong');
         assertEquals('1', $records[0]['t1']['value'], 'record 0 t1 value is wrong');
-        assertEquals('2', $records[0]['t2']['value'], 'record 0 t2 value is wrong');
-        assertEquals('3', $records[0]['t3']['value'], 'record 0 t3 value is wrong');
+        assertEquals(2, $records[0]['t2']['value'], 'record 0 t2 value is wrong');
+        assertEquals(3, $records[0]['t3']['value'], 'record 0 t3 value is wrong');
 
+        assertEquals(2, $records[1]['id']['value'], 'record 1 id value is wrong');
         assertEquals('4', $records[1]['t1']['value'], 'record 1 t1 value is wrong');
-        assertEquals('4', $records[1]['t2']['value'], 'record 1 t2 value is wrong');
-        assertEquals('5', $records[1]['t3']['value'], 'record 1 t3 value is wrong');
+        assertEquals(4, $records[1]['t2']['value'], 'record 1 t2 value is wrong');
+        assertEquals(5, $records[1]['t3']['value'], 'record 1 t3 value is wrong');
 
+        assertEquals(3, $records[2]['id']['value'], 'record 2 id value is wrong');
         assertEquals('5', $records[2]['t1']['value'], 'record 2 t1 value is wrong');
-        assertEquals('6', $records[2]['t2']['value'], 'record 2 t2 value is wrong');
-        assertEquals('7', $records[2]['t3']['value'], 'record 1 t3 value is wrong');
+        assertEquals(6, $records[2]['t2']['value'], 'record 2 t2 value is wrong');
+        assertEquals(7, $records[2]['t3']['value'], 'record 1 t3 value is wrong');
 
     }
 
