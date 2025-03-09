8e662c8bea51e4281a0f8cb8e5fa9d1a93767f45
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-09 11:09:13 -0400

    fix update test url

--- .tsuite/tests/XHRRecordUpdateTest.php
+++ .tsuite/tests/XHRRecordUpdateTest.php
@@ -9,7 +9,7 @@
         assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
 
         global $session_token;
-        $response = test_curl($properties['endpoint_url'] . '/xhr/record_update', array('record_name' => 'test', 'fields' => '{"t3":{"value":"7","type":"TEXT"}}', 'criteria' => '{"id":{"value":"1","type":"INT"}}'), true, $session_token);
+        $response = test_curl($properties['endpoint_url'] . '/xhr/update_record', array('record_name' => 'test', 'fields' => '{"t3":{"value":"7","type":"TEXT"}}', 'criteria' => '{"id":{"value":"1","type":"INT"}}'), true, $session_token);
 
         $response_arr = json_decode($response['response'], true);
 
