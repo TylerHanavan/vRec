885139b60b0d2a3b238b5b49cc3ffc186b01ef79
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-09 11:10:26 -0400

    add two more insert tests

--- .tsuite/tests/XHRRecordInsertTest.php
+++ .tsuite/tests/XHRRecordInsertTest.php
@@ -16,4 +16,36 @@
 
     }
 
+    function test_new_record_2($properties) {
+        assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
+
+        global $session_token;
+        $response = test_curl($properties['endpoint_url'] . '/new', array('table' => 'test', 't1' => 4, 't2' => 4, 't3' => 5), true, $session_token);
+
+        $response_arr = json_decode($response['response'], true);
+
+        assertEquals(200, $response['http_code'], 'http code mismatch');
+
+        assertTrue(isset($response_arr) && !empty($response_arr), 'response array empty');
+
+        assertEquals('success', $response_arr['xhr_response_status'], 'mismatching xhr_response_status');
+
+    }
+
+    function test_new_record_3($properties) {
+        assertEquals('localhost:1347', $properties['endpoint_url'], 'endpoint url mismatch');
+
+        global $session_token;
+        $response = test_curl($properties['endpoint_url'] . '/new', array('table' => 'test', 't1' => 5, 't2' => 6, 't3' => 7), true, $session_token);
+
+        $response_arr = json_decode($response['response'], true);
+
+        assertEquals(200, $response['http_code'], 'http code mismatch');
+
+        assertTrue(isset($response_arr) && !empty($response_arr), 'response array empty');
+
+        assertEquals('success', $response_arr['xhr_response_status'], 'mismatching xhr_response_status');
+
+    }
+
 ?>
