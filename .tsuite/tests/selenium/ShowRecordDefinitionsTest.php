607fddefd9353b72719b693b459b4fd90b8bc1ec
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-16 13:35:44 -0400

    use namespace for webDriverBy

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -1,12 +1,14 @@
 <?php
 
+    use Facebook\WebDriver\Remote\WebDriverBy;
+
     function test_selenium_1($properties) {
 
         $selenium = $properties['selenium'];
 
         $selenium->get($properties['endpoint_url']);
 
-        $login_element = $selenium->findElement(Facebook\WebDriver\Remote\WebDriverBy::id('login-link'));
+        $login_element = $selenium->findElement(WebDriverBy::id('login-link'));
 
         assertEquals('Login', $element->getText());
 
