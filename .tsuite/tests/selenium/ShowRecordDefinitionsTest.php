e1180bcb1fca4c03de1b62da106e1707ac0668b1
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-16 20:13:39 -0400

    manual sleep

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -9,7 +9,7 @@ function test_selenium_1($properties) {
 
     $selenium = $properties['selenium'];
 
-    $selenium->get($properties['endpoint_url'] . '/test');
+    $selenium->get('tsuite02:1347/test');
 
     echo $properties['endpoint_url'] . "\n";
 
@@ -18,13 +18,19 @@ function test_selenium_1($properties) {
     // Wait until a specific element is present (adjust the element's selector as needed)
     $selenium->wait(10, 500)->until(
         WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::tagName('body'))
-    );    
+    );
 
     echo "After wait\n";
 
     // You can check for JavaScript-rendered content here by waiting for an element or using executeScript
 
     echo $selenium->getPageSource();
+
+    sleep(20);
+
+    $element = WebDriverBy::tagName('body');
+
+    var_dump($element);
 
     echo "End of test";
 
