34ad008b3abf36a2a897889248ae1466aed3bb01
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-17 06:20:47 -0400

    check login link text

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -1,33 +1,33 @@
 <?php
 
-use Facebook\WebDriver\WebDriverBy;
-use Facebook\WebDriver\WebDriverExpectedCondition;
+    use Facebook\WebDriver\WebDriverBy;
+    use Facebook\WebDriver\WebDriverExpectedCondition;
 
-function test_selenium_1($properties) {
+    function test_selenium_1($properties) {
 
-    $url = 'http://' . $properties['endpoint_url'];
+        $url = 'http://' . $properties['endpoint_url'];
 
-    $selenium = $properties['selenium'];
+        $selenium = $properties['selenium'];
 
-    $selenium->get("$url/test");
+        $selenium->get("$url/test");
 
-    $selenium->wait(10, 500)->until(
-        function () use ($selenium) {
-            $elements = $selenium->findElements(WebDriverBy::cssSelector('div'));
-    
-            return count($elements) > 2;
-        },
-        'Error locating more than two div elements'
-    );    
-    
-    $selenium->wait(10, 500)->until(
-        function () use ($selenium) {
-            $elements = $selenium->findElements(WebDriverBy::id('login-link'));
-    
-            return count($elements) == 1;
-        },
-        'Error locating login-link anchor'
-    );
+        $selenium->wait(10, 500)->until(
+            function () use ($selenium) {
+                $elements = $selenium->findElements(WebDriverBy::cssSelector('div'));
+        
+                return count($elements) > 2;
+            },
+            'Error locating more than two div elements'
+        );    
+        
+        $selenium->wait(10, 500)->until(
+            function () use ($selenium) {
+                $elements = $selenium->findElements(WebDriverBy::id('login-link'));
+        
+                return count($elements) == 1 && $elements[0]->getText() == 'Logint';
+            },
+            'Error locating login-link anchor'
+        );
 
-}
+    }
 ?>
