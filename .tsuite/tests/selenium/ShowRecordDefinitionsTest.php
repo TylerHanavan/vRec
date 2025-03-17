3eb45a7c5f65656b8270473d9c8b4184f8549c27
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-17 06:28:13 -0400

    check for .modal

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -18,7 +18,7 @@
                 return count($elements) > 2;
             },
             'Error locating more than two div elements'
-        );    
+        );
         
         $selenium->wait(10, 500)->until(
             function () use ($selenium) {
@@ -26,8 +26,30 @@
         
                 return count($elements) == 1 && $elements[0]->getText() == 'Login';
             },
-            'Error locating login-link anchor'
+            'Error locating #login-link anchor'
         );
+        
+        $selenium->wait(10, 500)->until(
+            function () use ($selenium) {
+                $elements = $selenium->findElements(WebDriverBy::className('modal'));
+        
+                return count($elements) != 0;
+            },
+            'Error location .modal'
+        );
+        
+        $selenium->wait(10, 500)->until(
+            function () use ($selenium) {
+                $elements = $selenium->findElements(WebDriverBy::className('modal'));
+        
+                return count($elements) == 1;
+            },
+            'More than one .modal found'
+        );
+
+        $modal = $selenium->findElement(WebDriverBy::className('modal'));
+
+        echo $modal->getAttribute('style') . "\n";
 
     }
 ?>
