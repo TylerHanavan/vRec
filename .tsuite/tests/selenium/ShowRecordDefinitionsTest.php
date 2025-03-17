1a5a59482d16b05edcc03f0c59e904bfdeb1c540
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-17 06:12:37 -0400

    check for login-link anchor

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -5,17 +5,28 @@ use Facebook\WebDriver\WebDriverExpectedCondition;
 
 function test_selenium_1($properties) {
 
+    $url = $properties['endpoint_url'];
+
     $selenium = $properties['selenium'];
 
-    $selenium->get('http://tsuite02:1347/test');
+    $selenium->get("$url/test");
 
-    $selenium->wait(15, 500)->until(
+    $selenium->wait(10, 500)->until(
         function () use ($selenium) {
             $elements = $selenium->findElements(WebDriverBy::cssSelector('div'));
     
             return count($elements) > 2;
         },
         'Error locating more than two div elements'
+    );    
+    
+    $selenium->wait(10, 500)->until(
+        function () use ($selenium) {
+            $elements = $selenium->findElements(WebDriverBy::id('login-link'));
+    
+            return count($elements) == 1;
+        },
+        'Error locating login-link anchor'
     );
 
 }
