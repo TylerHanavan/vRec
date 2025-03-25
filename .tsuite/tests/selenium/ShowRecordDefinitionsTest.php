a8049b0e19345bbe491d57c34f8aef35a5bb141e
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-24 21:01:24 -0400

    check for type=password

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -232,7 +232,7 @@
             $password_input = $password_input[0];
 
         assertEquals('input', $password_input->getTagName(), "$element_name has wrong tag");
-        $k='type'; $v='text'; assertEquals($v, $password_input->getAttribute($k), "$element_name `$k` attribute is wrong");
+        $k='type'; $v='password'; assertEquals($v, $password_input->getAttribute($k), "$element_name `$k` attribute is wrong");
         $k='class'; $v='form-control'; assertEquals($v, $password_input->getAttribute($k), "$element_name `$k` attribute is wrong");
         $k='id'; $v='password'; assertEquals($v, $password_input->getAttribute($k), "$element_name `$k` attribute is wrong");
         $k='name'; $v='password'; assertEquals($v, $password_input->getAttribute($k), "$element_name `$k` attribute is wrong");
