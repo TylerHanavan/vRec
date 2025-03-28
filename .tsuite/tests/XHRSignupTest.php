c671f557c20df040ab5acded96ae911cc8a76f18
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-28 06:56:07 -0400

    remove password length checker

--- .tsuite/tests/XHRSignupTest.php
+++ .tsuite/tests/XHRSignupTest.php
@@ -46,14 +46,6 @@
         assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Invalid email format"}', $response['response'], 'response mismatch');
     }
 
-    function test_xhr_signup_5($properties) {
-
-        $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array('username' => 'kevin', 'email' => 'example@example.com', 'password' => '123456'), true);
-
-        assertEquals(400, $response['http_code'], 'http code mismatch');
-        assertEquals('{"xhr_response_type":"signup","xhr_response_status":"error","error":"Password must be at least 8 characters"}', $response['response'], 'response mismatch');
-    }
-
     function test_xhr_success_signup($properties) {
         $response = test_curl($properties['endpoint_url'] . '/xhr/signup', array('username' => 'username', 'email' => 'example@example.com', 'password' => '12345678'), true);
         assertEquals(200, $response['http_code'], 'http code mismatch');
