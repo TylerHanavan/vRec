4c6ec53e954c46592be890f195e4f8d93653ee20
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 21:02:01 -0400

    typo

--- .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
+++ .tsuite/tests/selenium/ShowRecordDefinitionsTest.php
@@ -265,7 +265,7 @@
                 WebDriverExpectedCondition::alertIsPresent(),
                 'Expected alert did not appear.'
             );
-            $alert = $driver->switchTo()->alert();
+            $alert = $selenium->switchTo()->alert();
             $alert->accept();
         } catch(Exception $e) {
             echo 'Alert generated as expected\n';
