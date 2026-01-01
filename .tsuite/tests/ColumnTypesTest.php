f3ec02bf7515fa1be6dded103addad063d7e18d7
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-01 16:29:25 -0500

    remove debug

--- .tsuite/tests/ColumnTypesTest.php
+++ .tsuite/tests/ColumnTypesTest.php
@@ -2,8 +2,6 @@
 
     function test_enums_1($properties) {
 
-        var_dump($properties);
-
         require_once($properties['INSTALL_LOCATION'] . "/lib/record/ColumnTypes.php");
         
         $enums = [
@@ -15,12 +13,12 @@
             'TEXT' => 5
         ];
 
-        /*foreach($enums as $k => $v) {
+        foreach($enums as $k => $v) {
             assertTrue(ColumnTypes::translate_string($k) === $v, "ColumnTypes::translate_string($k) did not resolve to $v");
             assertTrue(ColumnTypes::translate_string($v) === $v, "ColumnTypes::translate_string($v) did not resolve to $v");
             assertTrue(ColumnTypes::$k === $v, "ColumnTypes::$k did not resolve to $v");
             assertTrue(ColumnTypes::transate_id($v) === $k, "ColumnTypes::translate_id($v) did not resolve to $k");
-        }*/
+        }
 
     }
 
