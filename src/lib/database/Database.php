f7a1c8b0af5ec35e91859effe39126f140ab69cb
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-01 10:56:58 -0500

    update Database.php types to confirm with MySQLDatabase.php

--- src/lib/database/Database.php
+++ src/lib/database/Database.php
@@ -23,8 +23,8 @@ declare(strict_types=1);
         abstract public function show_record_definitions() : array;
 
         abstract public function get_column_declaration(string $field, int $type, int $length) : string;
-        abstract public function get_column_requires_length(int $type) : bool;
-        abstract public function get_column_quote_character(int $type) : string;
+        abstract public function get_column_requires_length(int|string $type) : bool;
+        abstract public function get_column_quote_character(int|string $type) : string;
 
     }
 
