b1cb8f1b7aa0ce0cf1e23fa23daf308f56f48de4
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-01 16:41:30 -0500

    test hook class

---
+++ .tsuite/tests/hook/HookTest.php
@@ -0,0 +1,12 @@
+<?php
+
+    function test_enums_1($properties) {
+
+        require_once($properties['INSTALL_LOCATION'] . "/lib/hook/Hook.php");
+        
+        $hook = new Hook('test_hook_1', []);
+
+        assertEquals($hook->get_function(), 'test_hook_1', 'function getter not right');
+
+    }
+?
