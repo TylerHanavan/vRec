1f070f34b862d04732750645078a918160ee2d41
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-01 16:23:31 -0500

    add ColumnTypes test

---
+++ .tsuite/tests/ColumnTypesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+    function test_enums_1($properties) {
+        
+        $enums = [
+            'INT' => 0,
+            'VARCHAR' => 1,
+            'BOOLEAN' => 2,
+            'DATE' => 3,
+            'TIMESTAMP' => 4,
+            'TEXT' => 5
+        ];
+
+        foreach($enums as $k => $v) {
+            assertTrue(ColumnTypes::translate_string($k) === $v, "ColumnTypes::translate_string($k) did not resolve to $v");
+            assertTrue(ColumnTypes::translate_string($v) === $v, "ColumnTypes::translate_string($v) did not resolve to $v");
+            assertTrue(ColumnTypes::$k === $v, "ColumnTypes::$k did not resolve to $v");
+            assertTrue(ColumnTypes::transate_id($v) === $k, "ColumnTypes::translate_id($v) did not resolve to $k");
+        }
+
+    }
+
+?>
