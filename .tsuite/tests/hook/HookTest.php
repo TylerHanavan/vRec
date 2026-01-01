d149179b1f75b64bd86cc79842663396b618cca7
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-01 16:58:55 -0500

    remove bad check

--- .tsuite/tests/hook/HookTest.php
+++ .tsuite/tests/hook/HookTest.php
@@ -10,7 +10,6 @@
 
         assertTrue($hook->get_conditions() !== null, 'get_conditions is null');
         assertTrue(is_array($hook->get_conditions()), 'get_conditions is not an array');
-        assertTrue(isset($hook->get_conditions()), 'get_conditions is not set');
         assertTrue(count($hook->get_conditions()) === 0, 'get_conditions is not empty');
 
         assertTrue($hook->can_call_hook([]) === true, 'can_call_hook was false for empty set of calling conditions');
