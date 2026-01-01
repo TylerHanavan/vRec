2b78d9fe50cd37edd7ddcb44bc732b9704afb76f
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-01 16:36:19 -0500

    test enums by name individually

--- .tsuite/tests/ColumnTypesTest.php
+++ .tsuite/tests/ColumnTypesTest.php
@@ -16,10 +16,14 @@
         foreach($enums as $k => $v) {
             assertTrue(ColumnTypes::translate_string($k) === $v, "ColumnTypes::translate_string($k) did not resolve to $v");
             assertTrue(ColumnTypes::translate_string($v) === $v, "ColumnTypes::translate_string($v) did not resolve to $v");
-            assertTrue(ColumnTypes::$$k === $v, "ColumnTypes::$k did not resolve to $v");
             assertTrue(ColumnTypes::transate_id($v) === $k, "ColumnTypes::translate_id($v) did not resolve to $k");
         }
 
+        assertTrue(ColumnTypes::INT         === 0, "ColumnTypes::INT did not resolve to 0");
+        assertTrue(ColumnTypes::VARCHAR     === 1, "ColumnTypes::INT did not resolve to 1");
+        assertTrue(ColumnTypes::BOOLEAN     === 2, "ColumnTypes::INT did not resolve to 2");
+        assertTrue(ColumnTypes::DATE        === 3, "ColumnTypes::INT did not resolve to 3");
+        assertTrue(ColumnTypes::TIMESTAMP   === 4, "ColumnTypes::INT did not resolve to 4");
+        assertTrue(ColumnTypes::TEXT        === 5, "ColumnTypes::INT did not resolve to 5");
     }
-
-?>
+?
