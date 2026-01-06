32667161ebfcb155d181e7c56822e5b85fa23109
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-06 08:04:07 -0500

    fix sort if two cells equal

--- src/lib/client/main.js
+++ src/lib/client/main.js
@@ -917,8 +917,7 @@ function sortTable(table, column, asc = true) {
         if (primaryResult === 0) {
             const aId = parseInt($(a).attr('xhr-record-id')) || 0;
             const bId = parseInt($(b).attr('xhr-record-id')) || 0;
-            console.log(aId + " " + bId);
-            return (aId > bId ? 1 : aId === bId ? 0 : -1) * dirModifier;
+            return (aId > bId ? 1 : -1);
         }
 
         return primaryResult * dirModifier;
