5c4abb83397ccb6f8f80a174a3bad0e9c7eb960c
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-07 18:50:36 -0500

    load show-records page from selenium

--- .tsuite/tests/selenium/ShowRecordsTest.php
+++ .tsuite/tests/selenium/ShowRecordsTest.php
@@ -5,7 +5,23 @@
 
     function test_show_records($properties) {
 
-        var_dump($properties['selenium']->manage()->getCookies());
+        //global $session_cookie; // Not actually needed...
+
+        $url = 'http://' . $properties['endpoint_url'] . '/admin/show-records?r=test';
+
+        $selenium->get("$url");
+
+        if($properties['tester']->has_driver_quit()) throw new Exception("Selenium driver quit prior to test");
+
+        /* Check that Selenium can find more than 4 elements */
+        $selenium->wait(10, 500)->until(
+            function () use ($selenium) {
+                $elements = $selenium->findElements(WebDriverBy::cssSelector('*'));
+        
+                return count($elements) > 4;
+            },
+            'Error locating five or more elements'
+        );
 
     }
 
