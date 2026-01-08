42d41b45b98d5be8e8bd76d6c2c405dbc3a2d9c9
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-07 21:18:25 -0500

    fix call to find add new record button

--- .tsuite/tests/selenium/ShowRecordsTest.php
+++ .tsuite/tests/selenium/ShowRecordsTest.php
@@ -16,7 +16,7 @@
         if($properties['tester']->has_driver_quit()) throw new Exception("Selenium driver quit prior to test");
 
         $add_new_record_button = $selenium->wait(10, 100)->until(
-            WebDriverExpectedCondition::presenceOfElementLocated($add_btn_selector),
+            WebDriverExpectedCondition::presenceOfElementLocated('button.btn-primary[data-target="#add-new-record-modal"]'),
             'Add New Record button not found'
         );
 
