454f344ea5b206f31b3d50508ea9ffa7f9e11380
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-07 21:03:39 -0500

    10ms polling => 25ms

--- .tsuite/tests/selenium/ShowRecordsTest.php
+++ .tsuite/tests/selenium/ShowRecordsTest.php
@@ -85,7 +85,7 @@
 
             $submit_button->click();
 
-            $selenium->wait(5, 10)->until(
+            $selenium->wait(5, 25)->until(
                 function () use ($input1) {
                     // We wait until the value attribute is empty
                     return $input1->getAttribute('value') === '';
