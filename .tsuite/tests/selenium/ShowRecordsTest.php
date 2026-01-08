739104f232e2be1730b6ba47d13f5b99cc6b9ffa
Tyler Hanavan <TylerHanavan@gmail.com>
2026-01-07 21:11:07 -0500

    change how we find elements

--- .tsuite/tests/selenium/ShowRecordsTest.php
+++ .tsuite/tests/selenium/ShowRecordsTest.php
@@ -15,41 +15,14 @@
 
         if($properties['tester']->has_driver_quit()) throw new Exception("Selenium driver quit prior to test");
 
-        /* Check that Selenium can find more than 4 elements */
-        $selenium->wait(10, 100)->until(
-            function () use ($selenium) {
-                $elements = $selenium->findElements(WebDriverBy::cssSelector('*'));
-        
-                return count($elements) > 4;
-            },
-            'Error locating five or more elements'
+        $add_new_record_button = $selenium->wait(10, 100)->until(
+            WebDriverExpectedCondition::presenceOfElementLocated($add_btn_selector),
+            'Add New Record button not found'
         );
 
-        $add_new_record_modals = $selenium->findElements(WebDriverBy::id('add-new-record-modal'));
-        
-        assertTrue(isset($add_new_record_modals) && is_array($add_new_record_modals) && sizeof($add_new_record_modals) === 1, 'the search for #add-new-record-modal did not yield one result');
+        $add_new_record_modal = $selenium->findElement(WebDriverBy::id('add-new-record-modal'));
 
-        $add_new_record_modal = $add_new_record_modals[0];
-
-        assertFalse($add_new_record_modal->isDisplayed(), 'modal is visible but expected it to be invisible');
-
-        $buttons = $selenium->findElements(WebDriverBy::className('btn-primary'));
-
-        assertTrue(isset($buttons) && is_array($buttons), 'buttons not set or is not array');
-
-        // Find button for Add New Record
-        $found_add_new_record_button = false;
-
-        $add_new_record_button = null;
-
-        foreach($buttons as $button) {
-            if($button->getText() === 'Add New Record') {
-                $found_add_new_record_button = true;
-                $add_new_record_button = $button;
-            }
-        }
-
-        assertTrue($found_add_new_record_button, '\'Add New Record\' button not found');
+        if(!isset($add_new_record_modal), 'Add New Record modal not found');
 
         $add_new_record_button->click();
 
